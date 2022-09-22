8d0a2239ca35d48f9dd26d0eb1688156a4f0461a
EdFRAX <41185804+EdFRAX@users.noreply.github.com>
2022-09-22 12:29:02 +0530

    Update contract.sol

--- ERC721A Contract/simple erc-721a/contract.sol
+++ ERC721A Contract/simple erc-721a/contract.sol
@@ -5,18 +5,29 @@ import "erc721a/contracts/ERC721A.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 //Simple ERC-721A Contract to show all NFTs on marketplaces at once.
-contract TheContract is ERC721A, Ownable {
+contract ContractName is ERC721A, Ownable {
 
-    string public baseURI = "ipfs://CID/";
-    uint256 maxsupply = 1000; //Supply will be equal to quantity.
+    string public baseURI = "ipfs://{CID}/";
+    string public baseExtension = ".json";
+    uint256 currentIndex = 1; //Make first tokenId = first json file.
+    uint256 maxsupply = 1000; //It will be equal to quantity.
 
-    constructor(
-        string memory _name,
-        string memory _symbol) 
-        
-        ERC721A(_name, _symbol) {
-        // It will mint the amount at the address that deployed this contract.
+    constructor(string memory _name, string memory _symbol) ERC721A(_name, _symbol) {
+        // It will mint the quantity at the address that deployed this contract.
         _mint(msg.sender, maxsupply);
     }
 
+    function _baseURI() internal view override returns (string memory) {
+        return baseURI;
+    }
+
+    function _startTokenId() internal view virtual override returns (uint256) {
+        return currentIndex;
+    }
+
+    function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
+        if (!_exists(tokenId)) revert URIQueryForNonexistentToken();
+        string memory currentBaseURI = _baseURI();
+        return bytes(currentBaseURI).length != 0 ? string(abi.encodePacked(baseURI, _toString(tokenId), baseExtension)) : '';
+    }
 }
